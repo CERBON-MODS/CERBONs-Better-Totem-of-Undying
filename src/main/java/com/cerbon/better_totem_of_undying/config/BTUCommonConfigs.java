ca970d8ee36bbfff259abd16f6c2d628360caed7
Felipe Cerboncini <felipecerboncinix@gmail.com>
2023-06-04 00:10:18 -0300

    Make default things about Totem Of Undying configurable

--- src/main/java/com/cerbon/better_totem_of_undying/config/BTUCommonConfigs.java
+++ src/main/java/com/cerbon/better_totem_of_undying/config/BTUCommonConfigs.java
@@ -6,8 +6,72 @@ public class BTUCommonConfigs {
     public static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
     public static final ForgeConfigSpec SPEC;
 
+    public static final ForgeConfigSpec.ConfigValue<Boolean> IS_MOD_ENABLED;
+    public static final ForgeConfigSpec.ConfigValue<Boolean> APPLY_EFFECTS_ONLY_WHEN_NEEDED;
+    public static final ForgeConfigSpec.ConfigValue<Boolean> ENABLE_FIRE_RESISTANCE;
+    public static final ForgeConfigSpec.ConfigValue<Integer> FIRE_RESISTANCE_DURATION;
+    public static final ForgeConfigSpec.ConfigValue<Boolean> ENABLE_REGENERATION;
+    public static final ForgeConfigSpec.ConfigValue<Integer> REGENERATION_DURATION;
+    public static final ForgeConfigSpec.ConfigValue<Integer> REGENERATION_AMPLIFIER;
+    public static final ForgeConfigSpec.ConfigValue<Boolean> ENABLE_ABSORPTION;
+    public static final ForgeConfigSpec.ConfigValue<Integer> ABSORPTION_DURATION;
+    public static final ForgeConfigSpec.ConfigValue<Integer> ABSORPTION_AMPLIFIER;
+    public static final ForgeConfigSpec.ConfigValue<Float> SET_HEALTH;
+    public static final ForgeConfigSpec.ConfigValue<Boolean> REMOVE_ALL_EFFECTS;
+
     static {
         BUILDER.push("Better Totem of Undying Configs");
+        IS_MOD_ENABLED = BUILDER
+                .comment("If false the mod will not change Minecraft Totem of Undying. DEFAULT: TRUE")
+                .define("Is Mod Enabled", true);
+
+        SET_HEALTH = BUILDER
+                .comment("This value sets the health Totem of Undying will give to the entity upon use. DEFAULT: 1.0")
+                .define("Set Health", 1.0F);
+
+        REMOVE_ALL_EFFECTS = BUILDER
+                .comment("When Totem of Undying is used it removes all previous effects you had. If set to false, it will keep all the effects you had before using the totem. DEFAULT: TRUE")
+                .define("Remove All Effects", true);
+
+
+        BUILDER.push("Effects");
+        APPLY_EFFECTS_ONLY_WHEN_NEEDED = BUILDER
+                .comment("If false it will apply all the effects at once, example: if you are not in fire you will receive the fire resistance effect but if it's set to true you will only receive the effect if you are in fire. DEFAULT: TRUE")
+                .define("Apply Effects Only When Needed", true);
+
+        ENABLE_FIRE_RESISTANCE = BUILDER
+                .comment("If false Totem of Undying will not give you fire resistance effect. DEFAULT: TRUE")
+                .define("Enable Fire Resistance", true);
+
+        FIRE_RESISTANCE_DURATION = BUILDER
+                .comment("Sets the duration of the fire resistance effect in ticks. DEFAULT: 800")
+                .define("Fire Resistance Duration", 800);
+
+        ENABLE_REGENERATION = BUILDER
+                .comment("If false Totem of Undying will not give you regeneration effect. DEFAULT: TRUE")
+                .define("Enable Regeneration", true);
+
+        REGENERATION_DURATION = BUILDER
+                .comment("Sets the duration of the regeneration effect in ticks. DEFAULT: 900")
+                .define("Regeneration Duration", 900);
+
+        REGENERATION_AMPLIFIER = BUILDER
+                .comment("Sets the amplifier of the regeneration effect. DEFAULT: 1")
+                .define("Regeneration Amplifier", 1);
+
+        ENABLE_ABSORPTION = BUILDER
+                .comment("If false Totem of Undying will not give you absorption effect. DEFAULT: TRUE")
+                .define("Enable Absorption", true);
+
+        ABSORPTION_DURATION = BUILDER
+                .comment("Sets the duration of the absorption effect in ticks. DEFAULT: 100")
+                .define("Absorption Duration", 100);
+
+        ABSORPTION_AMPLIFIER = BUILDER
+                .comment("Sets the amplifier of the absorption effect. DEFAULT: 1")
+                .define("Absorption Amplifier", 1);
+        BUILDER.pop();
+
 
         BUILDER.pop();
         SPEC = BUILDER.build();
---
+++ src/main/java/com/cerbon/better_totem_of_undying/mixin/entity/LivingEntityMixin.java
@@ -0,0 +1,67 @@
+package com.cerbon.better_totem_of_undying.mixin.entity;
+
+import com.cerbon.better_totem_of_undying.config.BTUCommonConfigs;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.level.Level;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(LivingEntity.class)
+public abstract class LivingEntityMixin extends Entity {
+    public LivingEntityMixin(EntityType<?> pEntityType, Level pLevel) {
+        super(pEntityType, pLevel);
+    }
+
+    @Shadow public abstract boolean addEffect(MobEffectInstance pEffectInstance);
+
+    @Shadow public abstract void setHealth(float pHealth);
+
+    @Shadow public abstract boolean removeAllEffects();
+
+    @Inject(method = "checkTotemDeathProtection", at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/world/entity/LivingEntity;setHealth(F)V",
+            ordinal = 0),
+            cancellable = true)
+    public void improveTotemOdUndying(DamageSource pDamageSource, CallbackInfoReturnable<Boolean> cir){
+        if (BTUCommonConfigs.IS_MOD_ENABLED.get()){
+            boolean isFireResistanceEffectEnabled = BTUCommonConfigs.ENABLE_FIRE_RESISTANCE.get();
+            boolean isRegenerationEffectEnabled = BTUCommonConfigs.ENABLE_REGENERATION.get();
+            boolean isAbsorptionEffectEnabled = BTUCommonConfigs.ENABLE_ABSORPTION.get();
+            int fireResistanceEffectDuration = BTUCommonConfigs.FIRE_RESISTANCE_DURATION.get();
+            int regenerationEffectDuration = BTUCommonConfigs.REGENERATION_DURATION.get();
+            int regenerationEffectAmplifier = BTUCommonConfigs.REGENERATION_AMPLIFIER.get();
+            int absorptionEffectDuration = BTUCommonConfigs.ABSORPTION_DURATION.get();
+            int absorptionEffectAmplifier = BTUCommonConfigs.ABSORPTION_AMPLIFIER.get();
+
+            this.setHealth(BTUCommonConfigs.SET_HEALTH.get());
+            if (BTUCommonConfigs.REMOVE_ALL_EFFECTS.get()){
+                this.removeAllEffects();
+            }
+
+            if (BTUCommonConfigs.APPLY_EFFECTS_ONLY_WHEN_NEEDED.get()){
+                if (isRegenerationEffectEnabled){
+                    this.addEffect(new MobEffectInstance(MobEffects.REGENERATION, regenerationEffectDuration, regenerationEffectAmplifier));
+                }
+                if (isAbsorptionEffectEnabled){
+                    this.addEffect(new MobEffectInstance(MobEffects.ABSORPTION, absorptionEffectDuration, absorptionEffectAmplifier));
+                }
+
+                if (this.isOnFire() && isFireResistanceEffectEnabled){
+                    this.addEffect(new MobEffectInstance(MobEffects.FIRE_RESISTANCE, fireResistanceEffectDuration, 0));
+                }
+
+                this.level.broadcastEntityEvent(this, (byte)35);
+            }
+            cir.setReturnValue(true);
+        }
+    }
+}
