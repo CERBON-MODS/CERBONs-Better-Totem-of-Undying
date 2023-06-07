4d33039115b838aec75a09f168d48a72c379036a
Felipe Cerboncini <felipecerboncinix@gmail.com>
2023-06-07 12:49:08 -0300

    Improve code readability

--- src/main/java/com/cerbon/better_totem_of_undying/mixin/entity/LivingEntityMixin.java
+++ src/main/java/com/cerbon/better_totem_of_undying/mixin/entity/LivingEntityMixin.java
@@ -41,86 +41,103 @@ public abstract class LivingEntityMixin extends Entity implements Attackable, ne
             cancellable = true)
     public void improveTotemOfUndying(DamageSource pDamageSource, CallbackInfoReturnable<Boolean> cir) {
         if (BTUCommonConfigs.IS_MOD_ENABLED.get()) {
-            boolean isFireResistanceEffectEnabled = BTUCommonConfigs.ENABLE_FIRE_RESISTANCE.get();
-            int fireResistanceEffectDuration = BTUCommonConfigs.FIRE_RESISTANCE_DURATION.get();
+            boolean isRemoveAllEffectsEnabled = BTUCommonConfigs.REMOVE_ALL_EFFECTS.get();
+            float health = BTUCommonConfigs.SET_HEALTH.get();
 
-            boolean isRegenerationEffectEnabled = BTUCommonConfigs.ENABLE_REGENERATION.get();
-            int regenerationEffectDuration = BTUCommonConfigs.REGENERATION_DURATION.get();
-            int regenerationEffectAmplifier = BTUCommonConfigs.REGENERATION_AMPLIFIER.get();
-
-            boolean isAbsorptionEffectEnabled = BTUCommonConfigs.ENABLE_ABSORPTION.get();
-            int absorptionEffectDuration = BTUCommonConfigs.ABSORPTION_DURATION.get();
-            int absorptionEffectAmplifier = BTUCommonConfigs.ABSORPTION_AMPLIFIER.get();
-
-            boolean isWaterBreathingEffectEnabled = BTUCommonConfigs.ENABLE_WATER_BREATHING.get();
-            int waterBreathingEffectDuration = BTUCommonConfigs.WATER_BREATHING_DURATION.get();
-
-            boolean isIncreaseFoodLevelEnabled = BTUCommonConfigs.ENABLE_INCREASE_FOOD_LEVEL.get();
-            int minimumFoodLevel = BTUCommonConfigs.MINIMUM_FOOD_LEVEL.get();
-            int setFoodLevel = BTUCommonConfigs.SET_FOOD_LEVEL.get();
-
-            boolean isDestroyBlocksWhenSuffocatingEnabled = BTUCommonConfigs.DESTROY_BLOCKS_WHEN_SUFFOCATING.get();
-            boolean isDestroyPowderSnowWhenFullyFrozenEnabled = BTUCommonConfigs.DESTROY_POWDER_SNOW_WHEN_FULLY_FROZEN.get();
-
-            this.setHealth(BTUCommonConfigs.SET_HEALTH.get());
-
-            if (BTUCommonConfigs.REMOVE_ALL_EFFECTS.get()) {
+            if (isRemoveAllEffectsEnabled) {
                 this.removeAllEffects();
             }
 
-            if (BTUCommonConfigs.APPLY_EFFECTS_ONLY_WHEN_NEEDED.get()) {
-                if (this.isOnFire() && isFireResistanceEffectEnabled) {
-                    this.addEffect(new MobEffectInstance(MobEffects.FIRE_RESISTANCE, fireResistanceEffectDuration, 0));
-                }
-                if (this.isInWaterOrBubble() && isWaterBreathingEffectEnabled) {
-                    this.addEffect(new MobEffectInstance(MobEffects.WATER_BREATHING, waterBreathingEffectDuration, 0));
-                }
-            } else {
-                if (isFireResistanceEffectEnabled) {
-                    this.addEffect(new MobEffectInstance(MobEffects.FIRE_RESISTANCE, fireResistanceEffectDuration, 0));
-                }
-                if (isWaterBreathingEffectEnabled) {
-                    this.addEffect(new MobEffectInstance(MobEffects.WATER_BREATHING, waterBreathingEffectDuration, 0));
-                }
-            }
+            this.setHealth(health);
+            this.applyTotemEffects();
+            this.increaseFoodLevel();
+            this.destroyBlocksWhenSuffocatingOrFullyFrozen();
 
-            if (isRegenerationEffectEnabled) {
-                this.addEffect(new MobEffectInstance(MobEffects.REGENERATION, regenerationEffectDuration, regenerationEffectAmplifier));
-            }
-            if (isAbsorptionEffectEnabled) {
-                this.addEffect(new MobEffectInstance(MobEffects.ABSORPTION, absorptionEffectDuration, absorptionEffectAmplifier));
-            }
-            if (this.getType() == EntityType.PLAYER && isIncreaseFoodLevelEnabled) {
-                Player player = (Player) (Object) this;
-                int foodLevel = player.getFoodData().getFoodLevel();
-                if (foodLevel <= minimumFoodLevel) {
-                    player.getFoodData().setFoodLevel(setFoodLevel);
-                }
-            }
-            if ((this.isInWall() && isDestroyBlocksWhenSuffocatingEnabled) || (this.isFullyFrozen() && isDestroyPowderSnowWhenFullyFrozenEnabled)) {
-                Level level = this.level;
-                BlockPos entityPosition = this.blockPosition();
-                BlockState blockAtEntityPosition = level.getBlockState(entityPosition);
-                BlockState blockAboveEntityPosition = level.getBlockState(entityPosition.above());
-
-                if (blockAtEntityPosition.getBlock() != Blocks.BEDROCK && blockAboveEntityPosition.getBlock() != Blocks.BEDROCK) {
-                    if (level.getBlockState(entityPosition.above(2)).getBlock() instanceof FallingBlock) {
-                        int i = 2;
-                        while (true){
-                            if (level.getBlockState(entityPosition.above(i)).getBlock() instanceof FallingBlock){
-                                level.destroyBlock(entityPosition.above(i), true);
-                                i++;
-                            }else{
-                                break;
-                            }
-                        }
-                    }
-                    level.destroyBlock(entityPosition, true);
-                    level.destroyBlock(entityPosition.above(), true);
-                }
-            }
             this.level.broadcastEntityEvent(this, (byte) 35);
             cir.setReturnValue(true);
+        }
+    }
+
+    private void applyTotemEffects(){
+        boolean isApplyEffectsOnlyWhenNeededEnabled = BTUCommonConfigs.APPLY_EFFECTS_ONLY_WHEN_NEEDED.get();
+
+        boolean isFireResistanceEffectEnabled = BTUCommonConfigs.ENABLE_FIRE_RESISTANCE.get();
+        int fireResistanceEffectDuration = BTUCommonConfigs.FIRE_RESISTANCE_DURATION.get();
+
+        boolean isRegenerationEffectEnabled = BTUCommonConfigs.ENABLE_REGENERATION.get();
+        int regenerationEffectDuration = BTUCommonConfigs.REGENERATION_DURATION.get();
+        int regenerationEffectAmplifier = BTUCommonConfigs.REGENERATION_AMPLIFIER.get();
+
+        boolean isAbsorptionEffectEnabled = BTUCommonConfigs.ENABLE_ABSORPTION.get();
+        int absorptionEffectDuration = BTUCommonConfigs.ABSORPTION_DURATION.get();
+        int absorptionEffectAmplifier = BTUCommonConfigs.ABSORPTION_AMPLIFIER.get();
+
+        boolean isWaterBreathingEffectEnabled = BTUCommonConfigs.ENABLE_WATER_BREATHING.get();
+        int waterBreathingEffectDuration = BTUCommonConfigs.WATER_BREATHING_DURATION.get();
+
+        if (isApplyEffectsOnlyWhenNeededEnabled) {
+            if (this.isOnFire() && isFireResistanceEffectEnabled) {
+                this.addEffect(new MobEffectInstance(MobEffects.FIRE_RESISTANCE, fireResistanceEffectDuration, 0));
+            }
+            if (this.isInWaterOrBubble() && isWaterBreathingEffectEnabled) {
+                this.addEffect(new MobEffectInstance(MobEffects.WATER_BREATHING, waterBreathingEffectDuration, 0));
+            }
+        } else {
+            if (isFireResistanceEffectEnabled) {
+                this.addEffect(new MobEffectInstance(MobEffects.FIRE_RESISTANCE, fireResistanceEffectDuration, 0));
+            }
+            if (isWaterBreathingEffectEnabled) {
+                this.addEffect(new MobEffectInstance(MobEffects.WATER_BREATHING, waterBreathingEffectDuration, 0));
+            }
+        }
+
+        if (isRegenerationEffectEnabled) {
+            this.addEffect(new MobEffectInstance(MobEffects.REGENERATION, regenerationEffectDuration, regenerationEffectAmplifier));
+        }
+        if (isAbsorptionEffectEnabled) {
+            this.addEffect(new MobEffectInstance(MobEffects.ABSORPTION, absorptionEffectDuration, absorptionEffectAmplifier));
+        }
+    }
+
+    private void increaseFoodLevel(){
+        boolean isIncreaseFoodLevelEnabled = BTUCommonConfigs.ENABLE_INCREASE_FOOD_LEVEL.get();
+        int minimumFoodLevel = BTUCommonConfigs.MINIMUM_FOOD_LEVEL.get();
+        int setFoodLevel = BTUCommonConfigs.SET_FOOD_LEVEL.get();
+
+        if (this.getType() == EntityType.PLAYER && isIncreaseFoodLevelEnabled) {
+            Player player = (Player) (Object) this;
+            int foodLevel = player.getFoodData().getFoodLevel();
+            if (foodLevel <= minimumFoodLevel) {
+                player.getFoodData().setFoodLevel(setFoodLevel);
+            }
+        }
+    }
+
+    private void destroyBlocksWhenSuffocatingOrFullyFrozen(){
+        boolean isDestroyBlocksWhenSuffocatingEnabled = BTUCommonConfigs.DESTROY_BLOCKS_WHEN_SUFFOCATING.get();
+        boolean isDestroyPowderSnowWhenFullyFrozenEnabled = BTUCommonConfigs.DESTROY_POWDER_SNOW_WHEN_FULLY_FROZEN.get();
+
+        if ((this.isInWall() && isDestroyBlocksWhenSuffocatingEnabled) || (this.isFullyFrozen() && isDestroyPowderSnowWhenFullyFrozenEnabled)) {
+            Level level = this.level;
+            BlockPos entityPosition = this.blockPosition();
+            BlockState blockAtEntityPosition = level.getBlockState(entityPosition);
+            BlockState blockAboveEntityPosition = level.getBlockState(entityPosition.above());
+
+            if (blockAtEntityPosition.getBlock() != Blocks.BEDROCK && blockAboveEntityPosition.getBlock() != Blocks.BEDROCK) {
+                if (level.getBlockState(entityPosition.above(2)).getBlock() instanceof FallingBlock) {
+                    int i = 2;
+                    while (true){
+                        if (level.getBlockState(entityPosition.above(i)).getBlock() instanceof FallingBlock){
+                            level.destroyBlock(entityPosition.above(i), true);
+                            i++;
+                        }else{
+                            break;
+                        }
+                    }
+                }
+                level.destroyBlock(entityPosition, true);
+                level.destroyBlock(entityPosition.above(), true);
+            }
         }
     }
 }
