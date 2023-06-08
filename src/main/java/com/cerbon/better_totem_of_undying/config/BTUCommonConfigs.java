a1739c3a652b85f46ae8abf3ca6b8efe9387e5b1
Felipe Cerboncini <felipecerboncinix@gmail.com>
2023-06-08 12:04:39 -0300

    Improve mod configs

--- src/main/java/com/cerbon/better_totem_of_undying/config/BTUCommonConfigs.java
+++ src/main/java/com/cerbon/better_totem_of_undying/config/BTUCommonConfigs.java
@@ -22,65 +22,24 @@ public class BTUCommonConfigs {
     public static final ForgeConfigSpec.ConfigValue<Boolean> ENABLE_INCREASE_FOOD_LEVEL;
     public static final ForgeConfigSpec.ConfigValue<Boolean> DESTROY_BLOCKS_WHEN_SUFFOCATING;
     public static final ForgeConfigSpec.ConfigValue<Boolean> DESTROY_POWDER_SNOW_WHEN_FULLY_FROZEN;
-    public static final ForgeConfigSpec.ConfigValue<Boolean> KNOCKBACK_MOBS_AWAY;
-    public static final ForgeConfigSpec.ConfigValue<Double> KNOCKBACK_RADIUS;
-    public static final ForgeConfigSpec.ConfigValue<Double> KNOCKBACK_STRENGTH;
+    public static final ForgeConfigSpec.ConfigValue<Boolean> KNOCK_BACK_MOBS_AWAY;
+    public static final ForgeConfigSpec.ConfigValue<Double> KNOCK_BACK_RADIUS;
+    public static final ForgeConfigSpec.ConfigValue<Double> KNOCK_BACK_STRENGTH;
     public static final ForgeConfigSpec.ConfigValue<Integer> MINIMUM_FOOD_LEVEL;
     public static final ForgeConfigSpec.ConfigValue<Integer> SET_FOOD_LEVEL;
     public static final ForgeConfigSpec.ConfigValue<Boolean> REMOVE_ALL_EFFECTS;
 
     static {
-        BUILDER.push("Better Totem of Undying Configs");
-        IS_MOD_ENABLED = BUILDER
-                .comment("If false the mod will not change Minecraft Totem of Undying. DEFAULT: TRUE")
-                .define("Is Mod Enabled", true);
-
+        BUILDER.push("Default Totem Abilities from Minecraft");
         SET_HEALTH = BUILDER
                 .comment("This value sets the health Totem of Undying will give to the entity upon use. DEFAULT: 1.0")
                 .define("Set Health", 1.0F);
-
-        ENABLE_INCREASE_FOOD_LEVEL = BUILDER
-                .comment("If false Totem Of Undying will not increase your food level. DEFAULT: TRUE")
-                .define("Increase Food Level", true);
-
-        MINIMUM_FOOD_LEVEL = BUILDER
-                .comment("Sets the minimum food level needed to Totem Of Undying increase food level. DEFAULT: <= 6")
-                .define("Minimum Food Level", 6);
-
-        SET_FOOD_LEVEL = BUILDER
-                .comment("Sets the food level that Totem Of Undying will give upon use. DEFAULT: 10 = Half bar")
-                .define("Set Food Level", 10);
-
-        DESTROY_BLOCKS_WHEN_SUFFOCATING = BUILDER
-                .comment("If false Totem Of Undying will not break blocks when you are suffocating. DEFAULT: TRUE")
-                .define("Destroy Blocks When Suffocating", true);
-
-        DESTROY_POWDER_SNOW_WHEN_FULLY_FROZEN = BUILDER
-                .comment("If false Totem of Undying will not break the powder snow when you die fully frozen. DEFAULT: TRUE")
-                .define("Destroy Powder Snow When Fully Frozen", true);
-
-        KNOCKBACK_MOBS_AWAY = BUILDER
-                .comment("If false Totem of Undying will not knockback mobs away. DEFAULT: TRUE")
-                .define("Knockback Mobs Away", true);
-
-        KNOCKBACK_RADIUS = BUILDER
-                .comment("Sets the radius where entities needs to be for the totem knockback them. DEFAULT: 3")
-                .define("Knockback Radius", 3.0D);
-
-        KNOCKBACK_STRENGTH = BUILDER
-                .comment("Sets the strength of the knockback. DEFAULT: 2.5")
-                .define("Knockback Strength", 2.5D);
 
         REMOVE_ALL_EFFECTS = BUILDER
                 .comment("When Totem of Undying is used it removes all previous effects you had. If set to false, it will keep all the effects you had before using the totem. DEFAULT: TRUE")
                 .define("Remove All Effects", true);
 
-
         BUILDER.push("Effects");
-        APPLY_EFFECTS_ONLY_WHEN_NEEDED = BUILDER
-                .comment("If false it will apply all the effects at once, example: if you are not in fire you will receive the fire resistance effect but if it's set to true you will only receive the effect if you are in fire. DEFAULT: TRUE")
-                .define("Apply Effects Only When Needed", true);
-
         ENABLE_FIRE_RESISTANCE = BUILDER
                 .comment("If false Totem of Undying will not give you fire resistance effect. DEFAULT: TRUE")
                 .define("Enable Fire Resistance", true);
@@ -113,6 +72,19 @@ public class BTUCommonConfigs {
                 .comment("Sets the amplifier of the absorption effect. DEFAULT: 1")
                 .define("Absorption Amplifier", 1);
 
+        IS_MOD_ENABLED = BUILDER
+                .comment("If false the mod will not change Minecraft Totem of Undying. DEFAULT: TRUE")
+                .define("Is Mod Enabled", true);
+        BUILDER.pop(2);
+
+
+        BUILDER.push("New Totem Abilities");
+
+        BUILDER.push("Effects");
+        APPLY_EFFECTS_ONLY_WHEN_NEEDED = BUILDER
+                .comment("If false it will apply all the effects at once, example: if you are not in fire you will receive the fire resistance effect but if it's set to true you will only receive the effect if you are on fire. DEFAULT: TRUE")
+                .define("Apply Effects Only When Needed", true);
+
         ENABLE_WATER_BREATHING = BUILDER
                 .comment("If false Totem of Undying will not give you water breathing effect. DEFAULT: TRUE")
                 .define("Enable Water Breathing", true);
@@ -122,8 +94,44 @@ public class BTUCommonConfigs {
                 .define("Water Breathing Duration", 800);
         BUILDER.pop();
 
+        BUILDER.push("Increase food level");
+        ENABLE_INCREASE_FOOD_LEVEL = BUILDER
+                .comment("If false Totem of Undying will not increase your food level. DEFAULT: TRUE")
+                .define("Increase Food Level", true);
 
+        MINIMUM_FOOD_LEVEL = BUILDER
+                .comment("Sets the minimum food level needed to Totem of Undying increase food level. DEFAULT: <= 6")
+                .define("Minimum Food Level", 6);
+
+        SET_FOOD_LEVEL = BUILDER
+                .comment("Sets the food level that Totem of Undying will give upon use. DEFAULT: 8")
+                .define("Set Food Level", 8);
         BUILDER.pop();
+
+        BUILDER.push("Destroy Blocks When Suffocating or Fully Frozen");
+        DESTROY_BLOCKS_WHEN_SUFFOCATING = BUILDER
+                .comment("If false Totem of Undying will not break blocks when you are suffocating. DEFAULT: TRUE")
+                .define("Destroy Blocks When Suffocating", true);
+
+        DESTROY_POWDER_SNOW_WHEN_FULLY_FROZEN = BUILDER
+                .comment("If false Totem of Undying will not break the powder snow when you die fully frozen. DEFAULT: TRUE")
+                .define("Destroy Powder Snow When Fully Frozen", true);
+        BUILDER.pop();
+
+        BUILDER.push("Knock Back Mobs Away");
+        KNOCK_BACK_MOBS_AWAY = BUILDER
+                .comment("If false Totem of Undying will not knock back mobs away. DEFAULT: TRUE")
+                .define("Knock Back Mobs Away", true);
+
+        KNOCK_BACK_RADIUS = BUILDER
+                .comment("Sets the radius where entities needs to be for the totem knock back them. DEFAULT: 3.0")
+                .define("Knock Back Radius", 3.0D);
+
+        KNOCK_BACK_STRENGTH = BUILDER
+                .comment("Sets the strength of the knock back. DEFAULT: 2.5")
+                .define("Knock Back Strength", 2.5D);
+        BUILDER.pop();
+
         SPEC = BUILDER.build();
     }
 }
