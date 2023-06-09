b4b7d49887976512592778bfd8ddab5444eb7dd8
Felipe Cerboncini <felipecerboncinix@gmail.com>
2023-06-09 14:33:33 -0300

    Remove unnecessary events

--- src/main/java/com/cerbon/better_totem_of_undying/BetterTotemOfUndyingMod.java
+++ src/main/java/com/cerbon/better_totem_of_undying/BetterTotemOfUndyingMod.java
@@ -1,17 +1,10 @@
 package com.cerbon.better_totem_of_undying;
 
 import com.cerbon.better_totem_of_undying.config.BTUCommonConfigs;
-import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.server.ServerStartingEvent;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(BetterTotemOfUndyingMod.MOD_ID)
 public class BetterTotemOfUndyingMod
@@ -20,23 +13,7 @@ public class BetterTotemOfUndyingMod
 
     public BetterTotemOfUndyingMod()
     {
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-        modEventBus.addListener(this::commonSetup);
-
         MinecraftForge.EVENT_BUS.register(this);
-
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, BTUCommonConfigs.SPEC, "better_totem_of_undying.toml");
-    }
-
-    private void commonSetup(final FMLCommonSetupEvent event) {}
-
-    @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event) {}
-
-    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class ClientModEvents
-    {
-        @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event) {}
     }
 }
