c547bb843bf5268438a791a27dba198a2f8b3ab5
Felipe Cerboncini <felipecerboncinix@gmail.com>
2023-06-12 13:05:49 -0300

    Create a class to put all mod constants

--- src/main/java/com/cerbon/better_totem_of_undying/BetterTotemOfUndyingMod.java
+++ src/main/java/com/cerbon/better_totem_of_undying/BetterTotemOfUndyingMod.java
@@ -1,19 +1,17 @@
 package com.cerbon.better_totem_of_undying;
 
 import com.cerbon.better_totem_of_undying.config.BTUCommonConfigs;
+import com.cerbon.better_totem_of_undying.utils.BTUConstants;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 
-@Mod(BetterTotemOfUndyingMod.MOD_ID)
-public class BetterTotemOfUndyingMod
-{
-    public static final String MOD_ID = "better_totem_of_undying";
+@Mod(BTUConstants.MOD_ID)
+public class BetterTotemOfUndyingMod {
 
-    public BetterTotemOfUndyingMod()
-    {
+    public BetterTotemOfUndyingMod() {
         MinecraftForge.EVENT_BUS.register(this);
-        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, BTUCommonConfigs.SPEC, "better_totem_of_undying.toml");
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, BTUCommonConfigs.SPEC, BTUConstants.CONFIG_NAME);
     }
 }
---
+++ src/main/java/com/cerbon/better_totem_of_undying/utils/BTUConstants.java
@@ -0,0 +1,6 @@
+package com.cerbon.better_totem_of_undying.utils;
+
+public class BTUConstants {
+    public static final String MOD_ID = "better_totem_of_undying";
+    public static final String CONFIG_NAME = MOD_ID + ".toml";
+}
