d20cba97a233eeef416a02a300dcd3b49e9f167e
Felipe Cerboncini <felipecerboncinix@gmail.com>
2023-07-13 21:13:04 -0300

    Add @Unique annotation to LivingEntityMixin to avoid conflict with other mods

--- src/main/java/com/cerbon/better_totem_of_undying/mixin/entity/LivingEntityMixin.java
+++ src/main/java/com/cerbon/better_totem_of_undying/mixin/entity/LivingEntityMixin.java
@@ -12,6 +12,7 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -19,16 +20,18 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Objects;
 
+@SuppressWarnings(value = "AddedMixinMembersNamePattern")
 @Mixin(value = LivingEntity.class, priority = 1100)
 public abstract class LivingEntityMixin extends Entity implements ILivingEntityMixin {
-    public long lastBlockPos;
+
+    @Unique private long lastBlockPos;
 
     public LivingEntityMixin(EntityType<?> pEntityType, Level pLevel) {
         super(pEntityType, pLevel);
     }
 
     @Inject(method = "checkTotemDeathProtection", at = @At("HEAD"), cancellable = true)
-    private void checkTotemDeathProtection(DamageSource pDamageSource, CallbackInfoReturnable<Boolean> cir) {
+    private void checkTotemDeathProtection(DamageSource pDamageSource, @NotNull CallbackInfoReturnable<Boolean> cir) {
         LivingEntity livingEntity = (LivingEntity) (Object) this;
         cir.setReturnValue(BTUUtils.canSaveFromDeath(livingEntity, pDamageSource));
     }
@@ -57,6 +60,7 @@ public abstract class LivingEntityMixin extends Entity implements ILivingEntityM
         }
     }
 
+    @Unique
     @Override
     public long getLastBlockPos() {
         return lastBlockPos;
