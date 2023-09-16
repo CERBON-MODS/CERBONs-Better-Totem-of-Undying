ede60fa0bb030ae3ff4e5938d3fbe4055c252fc2
Felipe Cerboncini <felipecerboncinix@gmail.com>
2023-09-16 13:53:38 -0300

    LivingEntityMixin adjustments to follow mixin standards

--- src/main/java/com/cerbon/better_totem_of_undying/mixin/entity/LivingEntityMixin.java
+++ src/main/java/com/cerbon/better_totem_of_undying/mixin/entity/LivingEntityMixin.java
@@ -12,6 +12,7 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -21,44 +22,45 @@ import java.util.Objects;
 
 @Mixin(value = LivingEntity.class, priority = 1100)
 public abstract class LivingEntityMixin extends Entity implements ILivingEntityMixin {
-    public long lastBlockPos;
+
+    @Unique public long better_totem_of_undying_lastBlockPos;
 
     public LivingEntityMixin(EntityType<?> pEntityType, Level pLevel) {
         super(pEntityType, pLevel);
     }
 
     @Inject(method = "checkTotemDeathProtection", at = @At("HEAD"), cancellable = true)
-    private void checkTotemDeathProtection(DamageSource pDamageSource, CallbackInfoReturnable<Boolean> cir) {
+    private void better_totem_of_undying_checkTotemDeathProtection(DamageSource pDamageSource, CallbackInfoReturnable<Boolean> cir) {
         LivingEntity livingEntity = (LivingEntity) (Object) this;
         cir.setReturnValue(BTUUtils.canSaveFromDeath(livingEntity, pDamageSource));
     }
 
     @Inject(method = "addAdditionalSaveData", at = @At("TAIL"))
-    public void addCustomData(@NotNull CompoundTag pCompound, CallbackInfo ci){
-        pCompound.putLong("LastBlockPos", this.lastBlockPos);
+    public void better_totem_of_undying_addCustomData(@NotNull CompoundTag pCompound, CallbackInfo ci){
+        pCompound.putLong("LastBlockPos", this.better_totem_of_undying_lastBlockPos);
     }
 
     @Inject(method = "readAdditionalSaveData", at = @At("TAIL"))
-    public void readCustomData(@NotNull CompoundTag pCompound, CallbackInfo ci){
-        this.lastBlockPos = pCompound.getLong("LastBlockPos");
+    public void better_totem_of_undying_readCustomData(@NotNull CompoundTag pCompound, CallbackInfo ci){
+        this.better_totem_of_undying_lastBlockPos = pCompound.getLong("LastBlockPos");
     }
 
     @Inject(method = "baseTick", at = @At("TAIL"))
-    public void saveEntityLastBlockPos(CallbackInfo ci) {
+    public void better_totem_of_undying_saveEntityLastBlockPos(CallbackInfo ci) {
         if (!this.level.isClientSide) {
             Level level = this.level;
             BlockPos currentPos = this.blockPosition();
             BlockState blockBelowEntityPos = level.getBlockState(currentPos.below());
             boolean isValidBlock = blockBelowEntityPos.isRedstoneConductor(level, currentPos.below());
 
-            if (!Objects.equals(this.lastBlockPos, currentPos.asLong()) && isValidBlock) {
-                this.lastBlockPos = currentPos.asLong();
+            if (!Objects.equals(this.better_totem_of_undying_lastBlockPos, currentPos.asLong()) && isValidBlock) {
+                this.better_totem_of_undying_lastBlockPos = currentPos.asLong();
             }
         }
     }
 
     @Override
-    public long getLastBlockPos() {
-        return lastBlockPos;
+    public long better_totem_of_undying_getLastBlockPos() {
+        return better_totem_of_undying_lastBlockPos;
     }
 }
