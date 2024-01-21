1cf2b39adc148e1d0877a0f22d79514bb0aa249f
Felipe <felipecerboncinix@gmail.com>
2024-01-21 10:47:10 -0300

    Renamed configs

--- Common/src/main/java/com/cerbon/better_totem_of_undying/config/custom/Curios.java
+++ Common/src/main/java/com/cerbon/better_totem_of_undying/config/custom/Curios.java
@@ -1,6 +1,6 @@
 package com.cerbon.better_totem_of_undying.config.custom;
 
 public class Curios {
-    public boolean canUseTotemFromCuriosSlot = true;
+    public boolean canUseTotemFromCharmSlot = true;
     public boolean displayTotemOnChest = true;
 }
