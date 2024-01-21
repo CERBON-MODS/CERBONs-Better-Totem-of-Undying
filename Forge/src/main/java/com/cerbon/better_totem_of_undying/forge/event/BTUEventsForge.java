8504e0803f4b6feed98114860869c5b9240e133c
Felipe <felipecerboncinix@gmail.com>
2024-01-21 01:03:25 -0300

    Rename class

--- Forge/src/main/java/com/cerbon/better_totem_of_undying/forge/event/BTUEventsForge.java
+++ Forge/src/main/java/com/cerbon/better_totem_of_undying/forge/event/BTUEventsForge.java
@@ -4,6 +4,6 @@ import com.cerbon.better_totem_of_undying.util.BTUConstants;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(modid = BTUConstants.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
-public class ModNameEventsForge {
+public class BTUEventsForge {
 
 }
